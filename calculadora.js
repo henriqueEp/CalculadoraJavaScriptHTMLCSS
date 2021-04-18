3abfbe5d0b805c99de2f0bd3393ef449e96628cb
henriqueEp <henrique_silvamacedo@hotmail.com>
2021-04-18 20:10:59 -0300

    calculadora js

---
+++ calculadora.js
@@ -0,0 +1,20 @@
+function insert(num) {
+    var numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + num;
+}
+function clean(){
+    document.getElementById('resultado').innerHTML = "";
+}
+function back(){
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substr(0, resultado.length -1)
+}
+function calcular(){
+    var resultado = document.getElementById('resultado').innerHTML;
+    if(resultado){
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    }
+    else {
+        window.alert('Nada para calcular')
+    }
+}
